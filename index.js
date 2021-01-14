5777b366517f2ac3f091fc0f4faeaf6ad891b8cf
497566070@qq.com <497566070@qq.com>
2021-01-14 13:43:27 +0800

    win

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 if (process.platform === 'darwin') {
 	module.exports = require('./lib/mac');
 } else if (process.platform === 'linux') {
-	module.exports = require('./lib/linux');
+	// module.exports = require('./lib/linux');
 } else {
 	module.exports = require('./lib/win');
 }
